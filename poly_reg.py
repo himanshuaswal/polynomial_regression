c5ac06321748e668586b2bb222675a19cc3a0793
himanshu aswal <haswal403@gmail.com>
2018-11-14 18:17:05 +0530

    load the data

--- poly_reg.py
+++ poly_reg.py
@@ -2,3 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+train_data = pd.read_csv("data.csv")
+X = train_data['Var_X'].values.reshape(-1,1)
+y = train_data['Var_Y'].values

c7e86984dc5a737e82c98d21a0ba72f9ef167a84
himanshu aswal <haswal403@gmail.com>
2018-11-14 18:17:54 +0530

    create polynomial features

--- poly_reg.py
+++ poly_reg.py
@@ -5,3 +5,5 @@ from sklearn.preprocessing import PolynomialFeatures
 train_data = pd.read_csv("data.csv")
 X = train_data['Var_X'].values.reshape(-1,1)
 y = train_data['Var_Y'].values
+poly_feat = PolynomialFeatures(degree = 4)
+X_poly = poly_feat.fit_transform(X)
